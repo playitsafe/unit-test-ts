b188f3b2d94da6d08386320f779fc655eaba8b66
Aaron <playitsafe@qq.com>
2023-12-09 16:23:39 +0800

    move checker into private functions

--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -2,7 +2,7 @@ export enum PasswordErrors {
   SHORT = "Password is too short!",
   NO_UPPER_CASE = "Upper case letter required!",
   NO_LOWER_CASE = "Lower case letter required!",
-  NO_NUMBER = "At least one number required!",
+  NO_NUM = "At least one number required!",
 }
 
 export interface CheckResult {
@@ -13,20 +13,47 @@ export interface CheckResult {
 export class PasswordChecker {
   public checkPassword(pw: string): CheckResult {
     const reasons: PasswordErrors[] = [];
-    if (pw.length < 8) {
-      reasons.push(PasswordErrors.SHORT);
-    }
-    if (pw === pw.toLowerCase()) {
-      reasons.push(PasswordErrors.NO_LOWER_CASE);
-    }
-
-    if (pw === pw.toUpperCase()) {
-      reasons.push(PasswordErrors.NO_UPPER_CASE);
-    }
+    this.checkLength(pw, reasons);
+    this.checkForUpper(pw, reasons);
+    this.checkForLower(pw, reasons);
 
     return {
       valid: reasons.length === 0,
       reasons,
     };
+  }
+
+  public checkAdminPassword(pw: string): CheckResult {
+    const basicCheck = this.checkPassword(pw);
+    this.checkForNum(pw, basicCheck.reasons);
+    return {
+      valid: basicCheck.reasons.length === 0,
+      reasons: basicCheck.reasons,
+    };
+  }
+
+  private checkLength(pw: string, reasons: PasswordErrors[]) {
+    if (pw.length < 8) {
+      reasons.push(PasswordErrors.SHORT);
+    }
+  }
+
+  private checkForUpper(pw: string, reasons: PasswordErrors[]) {
+    if (pw === pw.toUpperCase()) {
+      reasons.push(PasswordErrors.NO_UPPER_CASE);
+    }
+  }
+
+  private checkForLower(pw: string, reasons: PasswordErrors[]) {
+    if (pw === pw.toLowerCase()) {
+      reasons.push(PasswordErrors.NO_LOWER_CASE);
+    }
+  }
+
+  private checkForNum(pw: string, reasons: PasswordErrors[]) {
+    const hasNum = /\d/;
+    if (!hasNum.test(pw)) {
+      reasons.push(PasswordErrors.NO_NUM);
+    }
   }
 }
